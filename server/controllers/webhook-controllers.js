be279dacdbeaa354dbf91cdf552863075637b557
honzza <honzzadvorak@gmail.com>
2021-02-02 19:57:03 +0100

    webhook processing (U), whk sort

--- server/controllers/webhook-controllers.js
+++ server/controllers/webhook-controllers.js
@@ -49,8 +49,7 @@ const getWebhook = async (req, res, next) => {
   try {
     await newWebhook.save();
   } catch (err) {
-    const error = new HttpError("Creating activity hook failed", 500);
-    return next(error);
+    console.error(err);
   }
   res.sendStatus(200);
   next();
@@ -58,109 +57,130 @@ const getWebhook = async (req, res, next) => {
 
 const processWebhooks = async (req, res, next) => {
   try {
-    await WebHook.find().exec((err, newWebhook) => {
-      newWebhook.forEach(async (w) => {
-        try {
-          // Process webhook activity event
-          if (w.object_type === "activity") {
-            let activity;
-            let user;
-            switch (w.aspect_type) {
-              // Webhook type - CREATE
-              case "create":
-                // Find out if activity already exists
-                try {
-                  activity = await Activity.findOne({
-                    id: w.object_id,
-                  });
-                } catch (err) {
-                  console.error(err);
-                }
-                if (!activity) {
+    await WebHook.find()
+      .sort({ event_time: 1 })
+      .exec((err, newWebhook) => {
+        newWebhook.forEach(async (w) => {
+          try {
+            // Process webhook activity event
+            if (w.object_type === "activity") {
+              let activity;
+              let user;
+              switch (w.aspect_type) {
+                // Webhook type - CREATE
+                case "create":
+                  // Find out if activity already exists
                   try {
-                    user = await User.findOne({ uid: w.owner_id });
+                    activity = await Activity.findOne({
+                      id: w.object_id,
+                    });
                   } catch (err) {
                     console.error(err);
                   }
-                  // Refresh user token
-                  if (new Date() > user.expirationATC) {
+                  if (!activity) {
                     try {
-                      const tokens = await axios.post(
-                        `https://www.strava.com/api/v3/oauth/token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=refresh_token&refresh_token=${user.refreshTC}`
-                      );
-                      user.shortlivedATC = tokens.data.access_token;
-                      user.refreshTC = tokens.data.refresh_token;
-                      user.expirationATC = tokens.data.expires_at * 1000;
+                      user = await User.findOne({ uid: w.owner_id });
                     } catch (err) {
                       console.error(err);
                     }
-                    try {
-                      user.save();
-                    } catch (err) {
-                      console.error(err);
-                    }
-                  }
-                  // Get new activity with token up-to-date
-                  try {
-                    activity = await axios.get(
-                      `https://www.strava.com/api/v3/activities/${w.object_id}`,
-                      {
-                        headers: {
-                          Authorization: `Bearer ${user.shortlivedATC}`,
-                        },
+                    // Refresh user token
+                    if (new Date() > user.expirationATC) {
+                      try {
+                        const tokens = await axios.post(
+                          `https://www.strava.com/api/v3/oauth/token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=refresh_token&refresh_token=${user.refreshTC}`
+                        );
+                        user.shortlivedATC = tokens.data.access_token;
+                        user.refreshTC = tokens.data.refresh_token;
+                        user.expirationATC = tokens.data.expires_at * 1000;
+                      } catch (err) {
+                        console.error(err);
                       }
-                    );
+                      try {
+                        user.save();
+                      } catch (err) {
+                        console.error(err);
+                      }
+                    }
+                    // Get new activity with token up-to-date
+                    try {
+                      activity = await axios.get(
+                        `https://www.strava.com/api/v3/activities/${w.object_id}`,
+                        {
+                          headers: {
+                            Authorization: `Bearer ${user.shortlivedATC}`,
+                          },
+                        }
+                      );
+                    } catch (err) {
+                      console.error(err);
+                    }
+                    const ah = activity.data;
+                    const newActivity = {
+                      athlete_id: ah.athlete.id,
+                      name: ah.name,
+                      distance: ah.distance,
+                      moving_time: ah.moving_time,
+                      elapsed_time: ah.elapsed_time,
+                      total_elevation_gain: ah.total_elevation_gain,
+                      type: ah.type,
+                      id: ah.id,
+                      start_date_local: ah.start_date_local,
+                      start_latlng: ah.start_latlng,
+                      average_speed: ah.average_speed,
+                      max_speed: ah.max_speed,
+                      average_heartrate: ah.average_heartrate,
+                      max_heartrate: ah.max_heartrate,
+                      elev_high: ah.elev_high,
+                      elev_low: ah.elev_low,
+                    };
+                    try {
+                      activity = await Activity.create(newActivity);
+                      if (activity) await w.remove();
+                    } catch (err) {
+                      console.error(err);
+                    }
+                  }
+                  break;
+
+                // Webhook type - UPDATE
+                case "update":
+                  try {
+                    activity = await Activity.findOne({
+                      id: w.object_id,
+                    });
                   } catch (err) {
                     console.error(err);
                   }
-                  const ah = activity.data;
-                  const newActivity = {
-                    athlete_id: ah.athlete.id,
-                    name: ah.name,
-                    distance: ah.distance,
-                    moving_time: ah.moving_time,
-                    elapsed_time: ah.elapsed_time,
-                    total_elevation_gain: ah.total_elevation_gain,
-                    type: ah.type,
-                    id: ah.id,
-                    start_date_local: ah.start_date_local,
-                    start_latlng: ah.start_latlng,
-                    average_speed: ah.average_speed,
-                    max_speed: ah.max_speed,
-                    average_heartrate: ah.average_heartrate,
-                    max_heartrate: ah.max_heartrate,
-                    elev_high: ah.elev_high,
-                    elev_low: ah.elev_low,
-                  };
+                  if (activity) {
+                    if (w.updates.title) activity.name = w.updates.title;
+                    if (w.updates.type) activity.type = w.updates.type;
+                    if (!w.updates.private) await w.remove();
+                    try {
+                      await activity.save();
+                    } catch (err) {
+                      console.error(err);
+                    }
+                  }
+                  break;
+
+                // Webhook type - DELETE
+                case "delete":
                   try {
-                    activity = await Activity.create(newActivity);
+                    activity = await Activity.findOneAndDelete({
+                      id: w.object_id,
+                    });
                     if (activity) await w.remove();
                   } catch (err) {
                     console.error(err);
                   }
-                }
-                break;
-              case "update":
-                console.log("update");
-                break;
-              // Webhook type - DELETE
-              case "delete":
-                try {
-                  activity = await Activity.findOneAndDelete({
-                    id: w.object_id,
-                  });
-                  if (activity) await w.remove();
-                } catch (err) {
-                  console.error(err);
-                }
-                break;
+                  break;
+              }
             }
+          } catch (err) {
+            console.error(err);
           }
-        } catch (err) {
-          console.error(err);
-        }
+        });
       });
-    });
   } catch (err) {
     console.error(err);
   }
