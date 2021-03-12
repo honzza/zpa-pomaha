9aacd90c49ca2d7d9bca03069f2bb7f5375f8e6e
honzza <honzzadvorak@gmail.com>
2021-03-12 23:43:24 +0100

    2.5.2.5-rollback

--- server/config/winston.js
+++ server/config/winston.js
@@ -16,7 +16,6 @@ const options = {
 
 // instantiate a new Winston Logger with the settings defined above
 const userLogger = createLogger({
-  format: winston.format.json(),
   transports: [
     new transports.MongoDB({
       ...options.mongo,
