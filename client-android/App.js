0868a685e71e83a1dff0609ed0476bf419813f61
honzza <honzzadvorak@gmail.com>
2021-05-30 10:18:25 +0200

    0.0.2-RN reac native paper

--- client-android/App.js
+++ client-android/App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
+import { DataTable, Avatar } from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>MILUJU TĚ</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        marginTop: 40,
+      }}
+    >
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title style={{ justifyContent: "center" }}>
+            BĚH
+          </DataTable.Title>
+          <DataTable.Title style={{ justifyContent: "center" }}>
+            CYKLO
+          </DataTable.Title>
+          <DataTable.Title style={{ justifyContent: "center" }}>
+            BĚŽKY
+          </DataTable.Title>
+          <DataTable.Title style={{ justifyContent: "center" }}>
+            PLAVÁNÍ
+          </DataTable.Title>
+        </DataTable.Header>
+
+        <DataTable.Row>
+          <DataTable.Cell style={{ justifyContent: "center" }}>
+            2000 km
+          </DataTable.Cell>
+          <DataTable.Cell style={{ justifyContent: "center" }}>
+            3000 km
+          </DataTable.Cell>
+          <DataTable.Cell style={{ justifyContent: "center" }}>
+            700 km
+          </DataTable.Cell>
+          <DataTable.Cell style={{ justifyContent: "center" }}>
+            50 km
+          </DataTable.Cell>
+        </DataTable.Row>
+      </DataTable>
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title style={{ justifyContent: "center" }}>
+            Pořadí
+          </DataTable.Title>
+          <DataTable.Title style={{ justifyContent: "center" }}>
+            Avatar
+          </DataTable.Title>
+          <DataTable.Title style={{ justifyContent: "center" }}>
+            Sportovec
+          </DataTable.Title>
+          <DataTable.Title style={{ justifyContent: "center" }}>
+            Celkem
+          </DataTable.Title>
+        </DataTable.Header>
+
+        <DataTable.Row>
+          <DataTable.Cell style={{ justifyContent: "center" }}>
+            1
+          </DataTable.Cell>
+          <DataTable.Cell style={{ justifyContent: "center" }}>
+            <Avatar.Image
+              size={48}
+              source={{
+                uri: "https://lh3.googleusercontent.com/a-/AOh14GhFPD8xe0mSElwp4Bo9l9Or-DaOuMnAQIbwYiOPWw=s96-c",
+              }}
+            />
+          </DataTable.Cell>
+          <DataTable.Cell style={{ justifyContent: "center" }}>
+            honzza dvorak
+          </DataTable.Cell>
+          <DataTable.Cell style={{ justifyContent: "center" }}>
+            650 Kč
+          </DataTable.Cell>
+        </DataTable.Row>
+        <DataTable.Row></DataTable.Row>
+      </DataTable>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
